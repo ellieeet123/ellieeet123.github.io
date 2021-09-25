4719f50984c65257c46fa490e9c57c74e70b6151
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2021-09-25 13:26:52 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ function resizeIframe(obj){
   obj.height = (obj.contentWindow.document.body.scrollHeight + 20);
 }
 function setTitle(){
+  alert('hello')
   var titleData = document.getElementById('gameName').textContent + ' - Pringles';
   document.title = titleData;
   alert('hello')
