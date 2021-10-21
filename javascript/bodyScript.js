d95d1803b8888925fade06d0e1b938bb0b753ad6
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2021-10-21 17:04:28 -0500

    Update bodyScript.js

--- javascript/bodyScript.js
+++ javascript/bodyScript.js
@@ -1,5 +1,5 @@
 window.onload = setTitle()
-pageTitle = document.getElementById('title');
-if (pageTitle.textContent == 'Home') {
-  updateDark();
-}
+var pageTitle = document.getElementById('title');
+//if (pageTitle.textContent == 'Home') {
+//  updateDark();
+//}
