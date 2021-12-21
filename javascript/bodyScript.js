d5c7fa6215b57884fd735ae8b3943b3e4f5936fd
ellieeet123 <75391956+ellieeet123@users.noreply.github.com>
2021-12-21 10:22:39 -0600

    Update bodyScript.js

--- javascript/bodyScript.js
+++ javascript/bodyScript.js
@@ -2,17 +2,19 @@ var pageTitle = document.getElementById('title').textContent;
 if (!(window.location.href.endsWith('/games/'))) {
   document.title = pageTitle + ' - Pringles';
 }
-if (getCookie('updateLog_12_6_21') == '') {
+if (getCookie('newUrl_12_21_21') == '') {
   showMessage(`
-    <h1>Hello there</h1>
+    <h1>IMPORTANT</h1>
+    <p>PLEASE READ:</p>
     <br>
-    <p>There are a few new cool things on the site!</p>
-    <p>The biggest one is this new fancy popup thingy, its pretty cool</p>
-    <p>Also if you didn't know most of the html5 games were broken but now they are fixed.</p>
+    <p>Google has once again decided to mark the site I use to host some of the HTML5 games as malicious. I've made a new site again, and made some changes to it so that it is hopefully not marked as malicious again.</p>
+    <h1>But wait!</h1>
+    <p>Just incase something happens to this main URL as well, I've made a second backup URL for this site. I would recommend bookmarking it just incase this one is suddenly taken down, and you aren't able to access it to find the new URL.</p>
     <br>
-    <p>Also if you didn't know, as of about a week ago there are like 10 more color themes so if you want to check those out click <a href="/stuff/settings">here</a></p>
+    <p>The URL is <a href="https://pring-site.github.io">https://pring-site.github.io</a></p>
+    <p>You can also find it in the page called 'More Site URLS'. But keep in mind, if this site gets taken down you won't be able to find it there.</p>
     <br>
-    <p>Ok that's it, have a good day!</p>
+    <p>Once again. I highly recommend bookmarking the new URL, or writing it down somewhere.</p>
   `, 'Close');
-  setCookie('updateLog_12_6_21', 'seen', 31);
+  setCookie('newUrl_12_21_21', 'seen', 31);
 }
