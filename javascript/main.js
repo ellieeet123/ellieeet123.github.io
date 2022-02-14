e0103f118139d5f746c8f02e3c977fd71d374957
Nova <99283269+NovasGreat@users.noreply.github.com>
2022-02-14 15:23:01 -0600

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -860,7 +860,6 @@ function splashText() {
     'Now with more than 2 lines of code!',
     '',
     '01101110 01100101 01110110 01100101 01110010 00100000 01100111 01101111 01101110 01101110 01100001 00100000 01100111 01101001 01110110 01100101 00100000 01111001 01101111 01110101 00100000 01110101 01110000'
-    'There is about a 1/57 chance to get this message. If you DO get it then tell me! (aka Brandon)'
   ];
   var numGames = document.getElementById('sidebar').contentWindow.document.getElementById('games').getElementsByTagName('a').length;
   document.getElementById('splash').innerHTML = splashes[Math.floor(Math.random() * splashes.length)];
